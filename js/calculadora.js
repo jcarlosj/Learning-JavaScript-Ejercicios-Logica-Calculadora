711e1aae8ac865cd5cbcd17dc443d839ed341df5
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-03-08 12:33:20 -0500

    Refactoriza estructura del objeto 'calculadora'. Agrega eventos de teclado a la calculadora

--- js/calculadora.js
+++ js/calculadora.js
@@ -17,6 +17,10 @@ var calculadora = {
   resultado : false,      // Estado de la tecla igual (Si fue o no presionada)
   /* Función que inicializa la calculadora */
   inicio : function() {
+    calculadora .raton();     // Llama funcionalidades de la calculadora haciendo uso del mouse
+    calculadora .teclado();   // Llama funcionalidades de la calculadora haciendo uso del teclado
+  },
+  raton : function() {
     calculadora .teclas = document .querySelectorAll( '#calculadora #teclado ul li' );    // Obtiene todos los elementos que representan cada botón de la calculadora
     calculadora .pantalla = document .querySelector( '#calculadora #pantalla' );          // Obtiene el elemento que representa la pantalla de la calculadora
 
@@ -27,7 +31,93 @@ var calculadora = {
       // Crea un evento por cada elemento
       tecla .addEventListener( 'click', calculadora .presionar_click_mouse );
     });
+  },
+  teclado : function() {
+    // Agrega evento 'keydown' (presionar tecla) a todo el DOM
+    document .addEventListener( 'keydown', calculadora .presionar_tecla );
+  },
+  presionar_tecla: function( event ) {
+    console .log( event .keyCode );     // identifica el código de la tecla que se está lanzando el evento
 
+    // Capturamos el valor de la teclas de NÚMEROS de acuerdo al 'keycode' de la misma
+    if( event .keyCode == 48 || event .keyCode == 96 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 0;
+    }
+    if( event .keyCode == 49 || event .keyCode == 97 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 1;
+    }
+    if( event .keyCode == 50 || event .keyCode == 98 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 2;
+    }
+    if( event .keyCode == 51 || event .keyCode == 99 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 3;
+    }
+    if( event .keyCode == 52 || event .keyCode == 100 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 4;
+    }
+    if( event .keyCode == 53 || event .keyCode == 101 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 5;
+    }
+    if( event .keyCode == 54 || event .keyCode == 102 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 6;
+    }
+    if( event .keyCode == 55 || event .keyCode == 103 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 7;
+    }
+    if( event .keyCode == 56 || event .keyCode == 104 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 8;
+    }
+    if( event .keyCode == 57 || event .keyCode == 105 ) {
+      calculadora .accion = 'numero';
+      calculadora .valor = 9;
+    }
+    // Capturamos el valor de la teclas de SIGNOS de acuerdo al 'keycode' de la misma
+    if( event .keyCode == 171 || event .keyCode == 107 ) {
+      calculadora .accion = 'signo';
+      calculadora .valor = '+';
+    }
+    if( event .keyCode == 173 || event .keyCode == 109 ) {
+      calculadora .accion = 'signo';
+      calculadora .valor = '-';
+    }
+    if( event .keyCode == 106 ) {
+      calculadora .accion = 'signo';
+      calculadora .valor = '*';
+    }
+    if( event .keyCode == 111 ) {
+      calculadora .accion = 'signo';
+      calculadora .valor = '/';
+    }
+    // Capturamos el valor de la tecla de DECIMAL de acuerdo al 'keycode' de la misma
+    if( event .keyCode == 190 || event .keyCode == 110 ) {
+      calculadora .accion = 'decimal';
+      calculadora .valor = '.';
+    }
+    // Capturamos el valor de la tecla de IGUAL de acuerdo al 'keycode' de la misma
+    if( event .keyCode == 13 ) {
+      calculadora .accion = 'igual';
+      calculadora .valor = '=';
+    }
+    // Capturamos el valor de la tecla de ESC(27) BackSpace(8) de acuerdo al 'keycode' de la misma
+    if( event .keyCode == 8 || event .keyCode == 27 ) {
+      calculadora .accion = 'borrar';
+      calculadora .valor = '0';
+    }
+
+    /* NOTA: Tener en cuenta que la configuración del teclado para este ejercicio
+             es 'Español (Latinoamérica)' y los 'keyCode' capturará será solo para
+             este  */
+
+    calculadora .ejecutar( calculadora .accion, calculadora .valor );
   },
   /* Función que permite que cada evento pueda identificar a cada elemento o tecla de la calculadora a través de su atributo 'class'
      y determinar la acción a realizar y obtener el valor de cada elemento a través del evento programado para cada uno de ellos */
